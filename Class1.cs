94fde1663b717130c18994f176c0a672aa8c2202
egtph <40071220+egtph@users.noreply.github.com>
2020-09-21 23:05:18 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+
+namespace OutlookControl
+{
+    public class emailsend
+    {
+        public void ComposeEmail(string strTo, string strCC, string strSubject, string strHtmlBody, string strFolderEntryID, string strFolderStoreID)
+        {
+            Microsoft.Office.Interop.Outlook.Application app = new Microsoft.Office.Interop.Outlook.Application();
+
+            //Outlook.Folder DestFolder = (Folder)app.Session.GetFolderFromID(strFolderEntryID, strFolderStoreID);
+            Outlook.Folder DestFolder = app.Session.GetFolderFromID(strFolderEntryID, strFolderStoreID) as Outlook.Folder;
+
+            Outlook.MailItem item = app.CreateItem(Outlook.OlItemType.olMailItem);
+
+            item.HTMLBody = strHtmlBody;
+            item.To = strTo;
+            item.CC = strCC;
+            item.Subject = strSubject;
+
+            item.Display();
+            item.Save();
+            item.Move(DestFolder);
+
+        }
+    }
+}
